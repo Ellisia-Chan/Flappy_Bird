ec7695d71bb20077199d92973349932cd24ad872
Ellisia <christianjudevillaber@gmail.com>
2025-04-27 23:33:09 +0800

    added death anim

--- Assets/Script/Animation/Player/PlayerAnimation.cs
+++ Assets/Script/Animation/Player/PlayerAnimation.cs
@@ -1,6 +1,7 @@
 using System;
 using EventSystem;
 using EventSystem.Events;
+using SystemManagers;
 using UnityEngine;
 
 namespace Animation.Player {
@@ -8,23 +9,33 @@ namespace Animation.Player {
         private Animator animator;
 
         private Action<CoinCollectEvent> OnCoinCollect;
+        private Action<GameEventStateChange> OnGameEventStateChange;
 
         private void Awake() {
             animator = GetComponent<Animator>();
 
             OnCoinCollect = e => HandleCoinCollectAnim();
+            OnGameEventStateChange = e => HandleGameEventStateChange(e);
         }
 
         private void OnEnable() {
             EventBus.Subscribe(OnCoinCollect);
+            EventBus.Subscribe(OnGameEventStateChange);
         }
 
         private void OnDisable() {
             EventBus.Unsubscribe(OnCoinCollect);
+            EventBus.Unsubscribe(OnGameEventStateChange);
         }
 
         private void HandleCoinCollectAnim() {
             animator.SetTrigger("CoinCollect");
+        }
+
+        private void HandleGameEventStateChange(GameEventStateChange e) {
+            if (e.newState == GameManager.GameState.GAMEOVER) {
+                animator.SetTrigger("GameOver");
+            }
         }
     }
 
